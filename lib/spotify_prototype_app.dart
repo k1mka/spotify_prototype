05b0d2e1918f3976b318b9dc225a8a7a2c0543c3
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-01 18:55:05 +0300

    added to SpotifyPrototypeApp PlayListScreen

--- lib/spotify_prototype_app.dart
+++ lib/spotify_prototype_app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'presentation/screens/playlist_screen.dart';
+
 class SpotifyPrototypeApp extends StatelessWidget {
   const SpotifyPrototypeApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: PlayListScreen(),
+    );
   }
 }
