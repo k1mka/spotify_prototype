5c9157cc18227854f7adbf9285a3f5df3d5beafd
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-01 19:51:00 +0300

    created playlist widget

---
+++ lib/presentation/widgets/playlist_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_prototype/data/models/track_model.dart';
+import 'package:spotify_prototype/presentation/widgets/track_widget.dart';
+
+class PlayListWidget extends StatelessWidget {
+  const PlayListWidget({Key? key, required this.playlist}) : super(key: key);
+
+  final List<TrackModel> playlist;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: playlist.length,
+      itemBuilder: (_, i) => TrackWidget(
+        trackModel: playlist[i],
+      ),
+    );
+  }
+}
