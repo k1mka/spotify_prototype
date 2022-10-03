03471887099879b02f0f5f831327be129db07f30
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-03 13:19:54 +0300

    added states to layout

--- lib/presentation/screens/playlist_screen/playlist_layout.dart
+++ lib/presentation/screens/playlist_screen/playlist_layout.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spotify_prototype/presentation/screens/playlist_screen/bloc/tracklist_events.dart';
 import 'package:spotify_prototype/presentation/screens/playlist_screen/bloc/tracklist_states.dart';
 import 'package:spotify_prototype/presentation/screens/playlist_screen/bloc/traclist_bloc.dart';
+import 'package:spotify_prototype/presentation/widgets/track_widget.dart';
 
 class PlaylistLayout extends StatefulWidget {
   const PlaylistLayout({Key? key}) : super(key: key);
@@ -18,18 +19,30 @@ class _PlaylistLayoutState extends State<PlaylistLayout> {
     context.read<PLayListBloc>().add(LoadingTrackEvent());
   }
 
+  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PLayListBloc, PlayListState>(
-      builder: (context, state) {
-        //TODO : connect state and events
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.green,
-            title: const Text('Playlist'),
-          ),
-          body: Center(),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text('Playlist'),
+      ),
+      body: Center(
+        child: BlocBuilder<PLayListBloc, PlayListState>(
+          builder: (context, state) {
+            if (state is InitialState) return const Center(child: Text('expectation'));
+            if (state is ErrorTrack) return const Text('Error loading');
+            if (state is LoadingTrack) return const Center(child: CircularProgressIndicator());
+            if (state is LoadedTrack) {
+              return ListView.separated(
+                itemBuilder: (_, __) => const Divider(thickness: 4),
+                separatorBuilder: (__, index) => TrackWidget(trackModel: state.playList[index]),
+                itemCount: state.playList.length,
+              );
+            }
+            throw Exception('Not processed state in PlayListLayout');
+          },
+        ),
+      ),
     );
   }
 }
