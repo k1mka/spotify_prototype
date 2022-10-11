c21fe71e3c77a9d61a8beb3d26093f967a2f8c68
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-11 17:13:16 +0300

    created LikestScreen and layout

---
+++ lib/presentation/screens/likest_screen/likest_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_prototype/presentation/screens/likest_screen/likest_layout.dart';
+
+class LikestScreen extends StatelessWidget {
+  const LikestScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const LikestLayout();
+  }
+}
