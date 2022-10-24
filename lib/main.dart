a554c1929fa86ef26b5afa359bf46ce16e35d1d7
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-24 10:24:24 +0300

    added  WidgetsFlutterBinding.ensureInitialized();

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'get_it.dart';
 import 'spotify_prototype_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
   runApp(const SpotifyPrototypeApp());
 }
