d1e65589389ba02153115beab8465d55db865a1d
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-27 14:38:17 +0300

    created favouriteBloc

---
+++ lib/presentation/screens/likest_screen/bloc/favourite_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify_prototype/data/repository/repository.dart';
+import 'package:spotify_prototype/presentation/screens/likest_screen/bloc/favorite_events.dart';
+import 'package:spotify_prototype/presentation/screens/likest_screen/bloc/favorite_states.dsrt.dart';
+
+class FavoriteBloc extends Bloc<LoadedFavoriteList, LikestStates> {
+  Repository repo;
+  FavoriteBloc(this.repo) : super(InitialState()) {
+    on<LoadedFavoriteList>((event, emit) async {
+      emit(LoadingFavorite());
+      try {
+        emit(LoadedFavorite(await repo.fetchFavourite()));
+      } catch (e) {
+        emit(ErrorFavorite(e));
+      }
+    });
+  }
+}
