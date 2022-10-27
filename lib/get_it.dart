37ca6119ea9bf1212ffcb024638f31298d1f3e1c
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-27 14:40:11 +0300

    added dataStorageService dependencies to getit

--- lib/get_it.dart
+++ lib/get_it.dart
@@ -1,6 +1,9 @@
 import 'package:get_it/get_it.dart';
+import 'package:spotify_prototype/data/network_services/mocked_network_service.dart';
 import 'package:spotify_prototype/data/network_services/network_service.dart';
 import 'package:spotify_prototype/data/network_services/network_service_impl.dart';
+import 'package:spotify_prototype/data/network_services/storage_mocked_impl.dart';
+import 'package:spotify_prototype/data/network_services/storage_mocked_service.dart';
 import 'data/repository/repo_impl.dart';
 import 'data/repository/repository.dart';
 
@@ -8,8 +11,10 @@ final getIt = GetIt.instance;
 
 void setupServiceLocator() {
   final dataService = NetworkServiceImpl();
+  final dataStorageService = StorageMockedImpl();
   getIt.registerSingleton<NetworkService>(dataService);
+  getIt.registerSingleton<StorageMockedService>(dataStorageService);
   getIt.registerSingleton<Repository>(
-    RepoImpl(dataService),
+    RepoImpl(storageMockedService: dataStorageService, networkService: dataService),
   );
 }
