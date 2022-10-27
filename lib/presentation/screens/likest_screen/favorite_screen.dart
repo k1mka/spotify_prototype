b3ad39cc64306a3434d32f73a0fb65e9db1bd6ee
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-27 14:41:59 +0300

    connect bloc to the LikestScreen screeb

--- lib/presentation/screens/likest_screen/favorite_screen.dart
+++ lib/presentation/screens/likest_screen/favorite_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify_prototype/data/repository/repository.dart';
+import 'package:spotify_prototype/get_it.dart';
+import 'package:spotify_prototype/presentation/screens/likest_screen/bloc/favourite_bloc.dart';
 import 'package:spotify_prototype/presentation/screens/likest_screen/favorite_layout.dart';
 
 class LikestScreen extends StatelessWidget {
@@ -6,6 +10,9 @@ class LikestScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const FavoriteLayout();
+    return BlocProvider<FavoriteBloc>(
+      create: (context) => FavoriteBloc(getIt<Repository>()),
+      child: const FavoriteLayout(),
+    );
   }
 }
