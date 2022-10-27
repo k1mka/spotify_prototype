dca49c1f7493c03cbdd1e30547a39e46c96814ef
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-27 14:40:21 +0300

    refactored code

--- lib/presentation/screens/playlist_screen/playlist_screen.dart
+++ lib/presentation/screens/playlist_screen/playlist_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spotify_prototype/data/repository/repository.dart';
 import 'package:spotify_prototype/get_it.dart';
-import 'package:spotify_prototype/presentation/screens/playlist_screen/bloc/traclist_bloc.dart';
+import 'package:spotify_prototype/presentation/screens/playlist_screen/bloc/tracklist_bloc.dart';
 import 'package:spotify_prototype/presentation/screens/playlist_screen/playlist_layout.dart';
 
 class PlayListScreen extends StatefulWidget {
