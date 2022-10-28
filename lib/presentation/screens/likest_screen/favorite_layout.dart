ffd5ee7b6abe91bdbb0cf64a5d437ee26328834f
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-28 11:55:45 +0300

    added processing states to FavoriteLayout

--- lib/presentation/screens/likest_screen/favorite_layout.dart
+++ lib/presentation/screens/likest_screen/favorite_layout.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spotify_prototype/presentation/screens/likest_screen/bloc/favorite_states.dsrt.dart';
+import 'package:spotify_prototype/presentation/screens/likest_screen/bloc/favourite_bloc.dart';
+import 'package:spotify_prototype/presentation/widgets/track_widget.dart';
 
 class FavoriteLayout extends StatelessWidget {
   const FavoriteLayout({Key? key}) : super(key: key);
@@ -10,8 +14,29 @@ class FavoriteLayout extends StatelessWidget {
         title: const Text('Likest'),
         backgroundColor: Colors.green,
       ),
-      body: const Center(
-        child: Text('List of likest tracks'),
+      body: Column(
+        children: [
+          Center(
+            child: BlocBuilder<FavoriteBloc, FavouriteStates>(
+              builder: (BuildContext context, state) {
+                if (state is InitialState) return const Text('EXPECTATION');
+                if (state is LoadingFavorite) return const CircularProgressIndicator();
+                if (state is ErrorFavorite) return const Text('unhandled exception in FavoriteLayout');
+                if (state is LoadedFavorite) {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: state.favoriteList.length,
+                      itemBuilder: (_, index) => TrackWidget(
+                        trackModel: state.favoriteList[index],
+                      ),
+                    ),
+                  );
+                }
+                throw Exception('unprocessed state $state in FavouriteLayout');
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
