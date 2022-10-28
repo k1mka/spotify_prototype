8f94c8fa7c1c8ea404f105e3bc3846bf83a3951b
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-28 13:39:12 +0300

    renamed event

--- lib/presentation/screens/likest_screen/bloc/favourite_bloc.dart
+++ lib/presentation/screens/likest_screen/bloc/favourite_bloc.dart
@@ -3,10 +3,10 @@ import 'package:spotify_prototype/data/repository/repository.dart';
 import 'package:spotify_prototype/presentation/screens/likest_screen/bloc/favorite_events.dart';
 import 'package:spotify_prototype/presentation/screens/likest_screen/bloc/favorite_states.dsrt.dart';
 
-class FavoriteBloc extends Bloc<LoadedFavoriteList, FavouriteStates> {
+class FavoriteBloc extends Bloc<LoadedFavoriteEvent, FavouriteStates> {
   Repository repo;
   FavoriteBloc(this.repo) : super(InitialState()) {
-    on<LoadedFavoriteList>((event, emit) async {
+    on<LoadedFavoriteEvent>((event, emit) async {
       emit(LoadingFavorite());
       try {
         emit(LoadedFavorite(await repo.fetchFavourite()));
