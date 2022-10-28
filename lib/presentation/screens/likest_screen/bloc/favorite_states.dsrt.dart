b7c13e776017ae6cc0f4dee4b7c219df142fc16e
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-28 11:54:23 +0300

    renamed FavouriteStates

--- lib/presentation/screens/likest_screen/bloc/favorite_states.dsrt.dart
+++ lib/presentation/screens/likest_screen/bloc/favorite_states.dsrt.dart
@@ -1,17 +1,17 @@
 import 'package:spotify_prototype/data/models/track_model.dart';
 
-abstract class LikestStates {}
+abstract class FavouriteStates {}
 
-class InitialState extends LikestStates {}
+class InitialState extends FavouriteStates {}
 
-class LoadingFavorite extends LikestStates {}
+class LoadingFavorite extends FavouriteStates {}
 
-class LoadedFavorite extends LikestStates {
+class LoadedFavorite extends FavouriteStates {
   LoadedFavorite(this.favoriteList);
   final List<TrackModel> favoriteList;
 }
 
-class ErrorFavorite extends LikestStates {
+class ErrorFavorite extends FavouriteStates {
   ErrorFavorite(this.exception);
   final Object exception;
 }
--- lib/presentation/screens/likest_screen/bloc/favourite_bloc.dart
+++ lib/presentation/screens/likest_screen/bloc/favourite_bloc.dart
@@ -3,7 +3,7 @@ import 'package:spotify_prototype/data/repository/repository.dart';
 import 'package:spotify_prototype/presentation/screens/likest_screen/bloc/favorite_events.dart';
 import 'package:spotify_prototype/presentation/screens/likest_screen/bloc/favorite_states.dsrt.dart';
 
-class FavoriteBloc extends Bloc<LoadedFavoriteList, LikestStates> {
+class FavoriteBloc extends Bloc<LoadedFavoriteList, FavouriteStates> {
   Repository repo;
   FavoriteBloc(this.repo) : super(InitialState()) {
     on<LoadedFavoriteList>((event, emit) async {
