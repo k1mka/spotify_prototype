f1fb0196492738b8fd644f48bda4e65d329ddc69
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-30 21:04:47 +0200

    implements getSavedTracks

--- lib/presentation/screens/likest_screen/bloc/favourite_bloc.dart
+++ lib/presentation/screens/likest_screen/bloc/favourite_bloc.dart
@@ -3,13 +3,14 @@ import 'package:spotify_prototype/data/repository/repository.dart';
 import 'package:spotify_prototype/presentation/screens/likest_screen/bloc/favorite_events.dart';
 import 'package:spotify_prototype/presentation/screens/likest_screen/bloc/favorite_states.dsrt.dart';
 
-class FavoriteBloc extends Bloc<LoadedFavoriteEvent, FavouriteStates> {
+class FavoriteBloc extends Bloc<LikeTrackEvent, FavouriteStates> {
   Repository repo;
   FavoriteBloc(this.repo) : super(InitialState()) {
-    on<LoadedFavoriteEvent>((event, emit) async {
+    on<LikeTrackEvent>((event, emit) async {
       emit(LoadingFavorite());
       try {
-        emit(LoadedFavorite(await repo.fetchFavourite()));
+        final tracks = await repo.getSavedTracks();
+        emit(LoadedFavorite(tracks));
       } catch (e) {
         emit(ErrorFavorite(e));
       }
