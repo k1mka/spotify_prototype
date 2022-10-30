c8b9914ad823d118fa4c797818c10cbd89e4f306
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-30 21:03:09 +0200

    registered TrackStorageServiceImpl dependencie

--- lib/get_it.dart
+++ lib/get_it.dart
@@ -4,17 +4,26 @@ import 'package:spotify_prototype/data/network_services/network_service.dart';
 import 'package:spotify_prototype/data/network_services/network_service_impl.dart';
 import 'package:spotify_prototype/data/network_services/storage_mocked_impl.dart';
 import 'package:spotify_prototype/data/network_services/storage_mocked_service.dart';
-import 'data/repository/repo_impl.dart';
-import 'data/repository/repository.dart';
+
+import 'package:spotify_prototype/data/network_services/track_storage_servise.dart';
+import 'package:spotify_prototype/data/repository/repo_impl.dart';
+import 'package:spotify_prototype/data/repository/repository.dart';
 
 final getIt = GetIt.instance;
 
 void setupServiceLocator() {
   final dataService = MockedServiceImpl();
   final dataStorageService = StorageMockedImpl();
+  final trackStorage = TrackStorageServiceImpl();
   getIt.registerSingleton<NetworkService>(dataService);
   getIt.registerSingleton<StorageMockedService>(dataStorageService);
+  getIt.registerSingleton<TrackStorageService>(trackStorage);
+
   getIt.registerSingleton<Repository>(
-    RepoImpl(storageMockedService: dataStorageService, networkService: dataService),
+    RepoImpl(
+      storageMockedService: dataStorageService,
+      networkService: dataService,
+      trackStorageService: trackStorage,
+    ),
   );
 }
