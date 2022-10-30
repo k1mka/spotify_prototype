1716a44445fa3c3b2a06f278d000abe0a473a569
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-30 21:03:39 +0200

    created new event

--- lib/presentation/screens/likest_screen/bloc/favorite_events.dart
+++ lib/presentation/screens/likest_screen/bloc/favorite_events.dart
@@ -1,3 +1,11 @@
+import 'package:spotify_prototype/data/models/track_model.dart';
+
 abstract class FavoriteEvents {}
 
 class LoadedFavoriteEvent extends FavoriteEvents {}
+
+class LikeTrackEvent extends FavoriteEvents {
+  final TrackModel trackModel;
+
+  LikeTrackEvent(this.trackModel);
+}
