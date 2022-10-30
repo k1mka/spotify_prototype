7af7a25da7a3e219d4bb3b935e321375e45e54a1
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-30 22:39:53 +0200

    add LikeTrackEvent to FavoriteEvent

--- lib/presentation/screens/favorite_screen/bloc/favorite_events.dart
+++ lib/presentation/screens/favorite_screen/bloc/favorite_events.dart
@@ -1,11 +1,3 @@
-import 'package:spotify_prototype/data/models/track_model.dart';
-
 abstract class FavoriteEvent {}
 
 class LoadFavoriteEvent extends FavoriteEvent {}
-
-class LikeTrackEvent extends FavoriteEvent {
-  final TrackModel trackModel;
-
-  LikeTrackEvent(this.trackModel);
-}
