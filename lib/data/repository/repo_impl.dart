82c3372253f1740b3bbbbca60b184ed4fa04b28b
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-30 21:02:31 +0200

    override methods

--- lib/data/repository/repo_impl.dart
+++ lib/data/repository/repo_impl.dart
@@ -1,13 +1,15 @@
 import 'package:spotify_prototype/data/models/track_model.dart';
 import 'package:spotify_prototype/data/network_services/network_service.dart';
 import 'package:spotify_prototype/data/network_services/storage_mocked_service.dart';
+import 'package:spotify_prototype/data/network_services/track_storage_servise.dart';
 import 'package:spotify_prototype/data/repository/repository.dart';
 
 class RepoImpl extends Repository {
-  RepoImpl({required this.networkService, required this.storageMockedService});
+  RepoImpl({required this.networkService, required this.storageMockedService, required this.trackStorageService});
 
   final NetworkService networkService;
   final StorageMockedService storageMockedService;
+  final TrackStorageService trackStorageService;
 
   @override
   Future<List<TrackModel>> fetchTrack({
@@ -18,4 +20,10 @@ class RepoImpl extends Repository {
 
   @override
   Future<List<TrackModel>> fetchFavourite() => storageMockedService.fetchModel();
+
+  @override
+  Future<List<TrackModel>> getSavedTracks() async => await trackStorageService.getSavedTracks();
+
+  @override
+  Future<void> saveTrack(TrackModel trackModel) async => await trackStorageService.saveTrack(trackModel);
 }
