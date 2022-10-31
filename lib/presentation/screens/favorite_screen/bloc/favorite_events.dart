9145e5b8f7ec36f90a965f7a3b42266a7a871e61
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-10-31 17:13:13 +0200

    created new event for deleting model

--- lib/presentation/screens/favorite_screen/bloc/favorite_events.dart
+++ lib/presentation/screens/favorite_screen/bloc/favorite_events.dart
@@ -1,3 +1,10 @@
+import 'package:spotify_prototype/data/models/track_model.dart';
+
 abstract class FavoriteEvent {}
 
 class LoadFavoriteEvent extends FavoriteEvent {}
+
+class DeleteFavoriteEvent extends FavoriteEvent {
+  DeleteFavoriteEvent(this.model);
+  final TrackModel model;
+}
